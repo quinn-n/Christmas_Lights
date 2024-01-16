b8fb45246d8a0364bc918c40bd7e148948b75a68
Quinn N <88954051+quinn-n@users.noreply.github.com>
2024-01-16 16:08:00 -0500

    Got rid of `LEDPatternFactory` auto cleanup

--- led_patterns/patternfactory.h
+++ led_patterns/patternfactory.h
@@ -28,39 +28,34 @@ class LEDPatternFactory {
     }
 
     LEDPattern* build_pattern(Pattern pattern) {
-        // Cleanup old led pattern
-        if (current_pattern != NULL) {
-            delete current_pattern;
-            current_pattern = NULL;
-        }
-
         // Build new pattern
+        LEDPattern* p = NULL;
         if (pattern == Pattern::Snake) {
-            current_pattern = new Snake(leds, n_leds, random(1, 100), random(1, 3), random(15, 50), random(4, 10));
+            // p = new Snake(leds, n_leds, random(1, 100), random(1, 3), random(15, 50), random(4, 10));
+            p = new Snake(leds, n_leds, random(1, 100), 1, random(15, 50), random(4, 10));
         }
         else if (pattern == Pattern::OverlappedSnake) {
-            current_pattern = new OverlappedSnake(leds, n_leds, random(1, 3), random(15, 50), random(4, 10));
+            p = new OverlappedSnake(leds, n_leds, random(1, 3), random(15, 50), random(4, 10));
         }
         else if (pattern == Pattern::DoubleSnake) {
-            current_pattern = new DoubleSnake(leds, n_leds, random(10, 50), random(15, 50), random(4, 10));
+            p = new DoubleSnake(leds, n_leds, random(10, 50), random(15, 50), random(4, 10));
         }
         else if (pattern == Pattern::Breathe) {
-            current_pattern = new Breathe(leds, n_leds, random(5000, 20000), random(3, 8));
+            p = new Breathe(leds, n_leds, random(5000, 20000), random(3, 8));
         }
         else if (pattern == Pattern::OverlappedBreathe) {
-            current_pattern = new OverlappedBreathe(leds, n_leds, random(5000, 20000), random(3, 8));
+            p = new OverlappedBreathe(leds, n_leds, random(5000, 20000), random(3, 8));
         }
         else if (pattern == Pattern::RandomOne) {
-            current_pattern = new RandomOne(leds, n_leds, random(100, 200), random(300, 450));
+            p = new RandomOne(leds, n_leds, random(100, 200), random(300, 450));
         }
         else if (pattern == Pattern::RandomAll) {
-            current_pattern = new RandomAll(leds, n_leds, random(150, 500), random(4, 10));
+            p = new RandomAll(leds, n_leds, random(150, 500), random(4, 10));
         }
 
-        return current_pattern;
+        return p;
     }
     private:
-    LEDPattern* current_pattern = NULL;
     CRGB* leds = NULL;
     int n_leds = -1;
 };
